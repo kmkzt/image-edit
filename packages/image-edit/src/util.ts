dce7fdd9c0101a25df1098325c39c6808d250a54
kmkzt <info.pscreator@gmail.com>
2019-03-05 21:51:32 +0900

    Add load function utils

--- packages/image-edit/src/util.ts
+++ packages/image-edit/src/util.ts
@@ -36,51 +36,27 @@ interface DrawCanvasSize {
   imageHeight: number
 }
 
-const getObjectURL = (file: File | Blob): string | null =>
+const createObjectURL = (file: File | Blob): string | null =>
   window.URL ? window.URL.createObjectURL(file) : null
 
-async function draw(
-  originalFile: File,
-  {
-    width = 200,
-    height = 200,
-    fillStyle = 'transparent',
-    rotate = 0,
-    x = 0,
-    y = 0,
-    scaleX = 1,
-    scaleY = 1,
-    quality = 0.7,
-    mimeType = 'image/jpeg'
-  }: any
-) {
-  try {
-    const canvas: HTMLCanvasElement = document.createElement('canvas')
-    const context: CanvasRenderingContext2D | null = canvas.getContext('2d')
-    const image = new Image()
-    image.onload = async () => {
-      const file: FileInfo = await loadFile(originalFile)
-      image.src = file.url
-    }
+const revokeObjectURL = (src: string) =>
+  window.URL ? window.URL.revokeObjectURL(src) : null
 
-    if (!context) return
-    canvas.width = width
-    canvas.height = height
-    context.fillStyle = fillStyle
-    context.fillRect(0, 0, width, height)
-    context.save()
-    context.translate(width / 2, height / 2)
-    context.rotate((rotate * Math.PI) / 180)
-    context.scale(scaleX, scaleY)
-    context.drawImage(image, x, y, width, height)
-    context.restore()
-    const getBlob = (blob: Blob | null) => {
-      return blob
+async function loadImage(file: FileInfo): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image: HTMLImageElement = new Image()
+    image.onload = () => {
+      resolve(image)
     }
-    canvas.toBlob(getBlob, mimeType, quality)
-  } catch (err) {
-    throw err
-  }
+    image.onabort = () => {
+      reject(new Error('Aborted to load the image.'))
+    }
+    image.onerror = () => {
+      reject(new Error('Failed to load the image.'))
+    }
+    image.alt = file.name
+    image.src = file.url
+  })
 }
 
 async function loadFile(file: File, cancel?: any): Promise<FileInfo> {
@@ -91,7 +67,7 @@ async function loadFile(file: File, cancel?: any): Promise<FileInfo> {
         const mimeType: string = file.type
         const url =
           file.type === 'image/jpeg'
-            ? URL.createObjectURL(file)
+            ? createObjectURL(file)
             : (e.target as any).result
         resolve({
           name: file.name,
@@ -107,6 +83,43 @@ async function loadFile(file: File, cancel?: any): Promise<FileInfo> {
     } else {
       reject(new Error('Failed to read the image with FileReader.'))
     }
+  })
+}
+
+async function createBlob(
+  image: HTMLImageElement,
+  {
+    fillStyle = 'transparent',
+    rotate = 0,
+    x = 0,
+    y = 0,
+    scaleX = 1,
+    scaleY = 1,
+    quality = 0.7,
+    mimeType = 'image/jpeg'
+  }: any
+): Promise<Blob | null> {
+  return new Promise((resolve, reject) => {
+    const canvas: HTMLCanvasElement = document.createElement('canvas')
+    const context: CanvasRenderingContext2D | null = canvas.getContext('2d')
+    if (!context) {
+      reject(new Error('Not Supported Canvas'))
+      return
+    }
+    const width = image.naturalWidth
+    const height = image.naturalHeight
+    canvas.width = width
+    canvas.height = height
+    context.fillStyle = fillStyle
+    context.fillRect(0, 0, width, height)
+    context.save()
+    context.translate(width / 2, height / 2)
+    context.rotate((rotate * Math.PI) / 180)
+    context.scale(scaleX, scaleY)
+    context.drawImage(image, x, y, width, height)
+    context.restore()
+    const blobSuccess = (blob: Blob | null) => resolve(blob)
+    canvas.toBlob(blobSuccess, mimeType, quality)
   })
 }
 
