8d7d281085bcbb64690fa8c8db623a5bf6185367
kmkzt <info.pscreator@gmail.com>
2019-03-10 01:51:16 +0900

    Update react-ui example

--- packages/react-image-edit/src/example/index.tsx
+++ packages/react-image-edit/src/example/index.tsx
@@ -1,15 +1,26 @@
 import React, { useCallback, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { FileDrop, DisplayFile } from '@/components/'
+import edit, { FileInfo, createBlob, loadImage } from 'image-edit'
 
 function App({}) {
   const [files, setFiles] = useState<File[]>([])
-
+  const [editImage, changeEdit] = useState<string | null>()
   const handleFile = useCallback(
     (f: File | FileList) => {
       setFiles([...files, ...Object.values(f)])
     },
     [files]
+  )
+  const selectFile = useCallback(
+    (i: number) => async (e: React.MouseEvent) => {
+      const image: string = await edit(files[i], {
+        quality: 0.7,
+        rotate: 10
+      } as any)
+      changeEdit(image)
+    },
+    [files, editImage]
   )
   return (
     <>
@@ -27,9 +38,11 @@ function App({}) {
             key={i}
             style={{ width: '100%', heigth: 'auto' }}
             file={file}
+            onClick={selectFile(i)}
           />
         ))}
       </div>
+      {editImage && <img src={editImage} />}
     </>
   )
 }
