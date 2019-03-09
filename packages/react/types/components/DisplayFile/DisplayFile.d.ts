4bf154955327ac2ae4512127a373aea975f58b15
kmkzt <info.pscreator@gmail.com>
2019-03-10 00:26:39 +0900

    react-image-edit build

---
+++ packages/react/types/components/DisplayFile/DisplayFile.d.ts
@@ -0,0 +1,6 @@
+import React, { HTMLAttributes } from 'react';
+interface Props {
+    file: File;
+}
+export declare const DisplayFile: React.FunctionComponent<Props & HTMLAttributes<HTMLImageElement>>;
+export {};
