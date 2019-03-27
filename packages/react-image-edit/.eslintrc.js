558b4116d7a8fefd6ff0bd7bc47d2974f24a711a
kmkzt <info.pscreator@gmail.com>
2019-03-27 18:16:52 +0900

    Add eslint-plugin-react config

--- packages/react-image-edit/.eslintrc.js
+++ packages/react-image-edit/.eslintrc.js
@@ -5,11 +5,8 @@ module.exports = {
     commonjs: true,
     es6: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'prettier/@typescript-eslint'
-  ],
-  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks',],
+  extends: ['plugin:react/recommended', 'prettier/@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
@@ -18,8 +15,25 @@ module.exports = {
       jsx: true
     }
   },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React', // Pragma to use, default to "React"
+      version: '16.8'
+    },
+    propWrapperFunctions: [
+      'forbidExtraProps',
+      { property: 'freeze', object: 'Object' },
+      { property: 'myFavoriteWrapper' }
+    ],
+    linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }]
+  },
   rules: {
+    // react-config
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
     'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     strict: 'error',
     'no-comma-dangle': false,
     'no-unused-vars': [1, { vars: 'all', args: 'after-used' }],
